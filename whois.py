554052999a29f3c2f7af1bdcf4bf0f07bf802c68
mithlesh02 <46572883+mithlesh02@users.noreply.github.com>
2020-01-26 11:35:21 +0530

    change

--- whois.py
+++ whois.py
@@ -1,8 +1,30 @@
-# following code brings whois record for a given ip address
 from ipwhois import IPWhois
+import sys 
+import ipwhois
+    
+ #Enter your ip address here 
 
-obj = IPWhois('139.99.159.15') #Enter your ip address here 
 
-res=obj.lookup_whois()
+try:
+  obj = IPWhois('10.21.131.7')
+except ipwhois.exceptions.IPDefinedError:
+  res = {}
+else:
+  res = obj.lookup()
 
-print(res)
+if (res=={}):
+	print('Private Network')
+	sys.exit()
+j=1
+for i in res:
+	
+	if(i=='nets'):
+		print(j,i," :    ",type(res[i]))
+		str1=str(res[i])
+		str1=str1.split(",")
+		for k in str1:
+			print('         ',k)
+	else:
+		print(j,i,' : ',res[i])
+
+	j=j+1
